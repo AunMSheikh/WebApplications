85ff3013aec623c5f18057b747c84dc062eddef5
Aun Sheikh <aun.m.sheikh@gmail.com>
2017-05-08 13:33:50 -0400

    Add files via upload

---
+++ view/login.php
@@ -0,0 +1,55 @@
+<DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<!--<meta name="viewport" content="width=device-width, initial-scale=1"> -->
+		<!--<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css"> -->
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<title>iGetIt</title>
+	</head>
+	<body>	
+		
+
+		<div class="form-login">
+			<h1 class="title"> iGetIt Login In </h1>
+			<div class="form">
+			 <form>
+                                        <fieldset>
+                                                
+                                                <p> <label for="user">User</label><input type="text" name="user"></input></p>
+                                                <p> <label for="password">Password</label><input type="password" name="password"></input> </p>
+                                                <p> <input type="submit" value="Log In"/>
+                                        	<div class="sign-up"> Sign Up </div>
+					</fieldset>  
+			</form>
+			</div>
+
+		</div>
+			
+	<!--	<nav>
+			<ul>
+			<li> <a href="">Class</a>
+			<li> <a href="">Profile</a>
+			<li> <a href="">Logout</a>
+			</ul>
+		</nav>
+	
+		<main>
+			<div class="center-class">
+	
+				<form class="login-form">
+					<fieldset>
+						<legend>Login</legend>
+						<p> <label for="user">User</label>    <input type="text" name="user"></input> </p>
+						<p> <label for="password">Password</label><input type="password" name="password"></input> </p>
+						<p> <input type="submit" />
+					</fieldset>
+				</form>
+			</div>
+		</main>
+		<footer>
+		</footer>
+	-->
+	</body>
+</html>
+
